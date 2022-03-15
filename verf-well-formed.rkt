bb0cead84daf7782094acceb31b98ef3f6995bfc
dfreita4s <danifp19@gmail.com>
2022-03-14 23:47:51 -0300

    verificacao se consome funcionando (eu acho)

--- verf-well-formed.rkt
+++ verf-well-formed.rkt
@@ -14,28 +14,54 @@
   
   )
 
-;; Usar funcao auxiliar para responder com os termos do ford
+;; Helpers functions 
+ ; If not consumed return false 
 
-#;(define (verf-ford grammar exp)
-  
+(define (choice e1 e2)
+  (if (member (term 0) (list e1))
+      #f
+      #t)
   )
 
-; se nao consumir é falso
-;; (* (/ (! ε) ε)) const
-;testar se a resposta da outra tem o 0
-(define (zero⇀? grammar e non-terminal) ;; acho que precisa fazer de algum jeito que use os (f, s, 1, 0) 
+(define (sequence e1 e2) 
+  (if (and (member (term 0) (list e1) ) (member (term 0) (list e2)))
+      #f
+      #t)
+  )
+
+(define (not-predicate e)
+  (if (member (term f) (list e))
+      #f
+      #t
+      )
+  )
+
+(define (kleene e)
+  (if (or (member (term 0) (list e)) (member (term f) (list e)))
+      #f
+      #t
+      )
+  )
+
+
+;; Analysis of Grammars
+;; If e ⇀ 0, then e might succeed while consuming no input
+;; If e ⇀ 1, then e might succeed while consuming at least one terminal
+;; If e ⇀ f, then e might fail on some input
+;; If e ⇀ s, then e outcome of either 0 or 1
+(define (zero⇀? grammar e non-terminal) 
   (if (list? e)
       (let ((id (car e)))
-        (cond [(eq? id '/)  (or (zero⇀? grammar (cadr e)) (zero⇀? grammar (caddr e)))]
-              [(eq? id '•)  (or (zero⇀? grammar (cadr e)) (zero⇀? grammar (caddr e)))]
-              [(eq? id '!)  (zero⇀? grammar (cadr e))]
-              [(eq? id '*)  (zero⇀? grammar (cadr e))]
+        (cond [(eq? id '/)  (choice (zero⇀? grammar (cadr e) non-terminal) (zero⇀? grammar (caddr e) non-terminal))]
+              [(eq? id '•)  (sequence (zero⇀? grammar (cadr e) non-terminal) (zero⇀? grammar (caddr e) non-terminal))]
+              [(eq? id '!)  (not-predicate (zero⇀? grammar (cadr e) non-terminal))]
+              [(eq? id '*)  (kleene (zero⇀? grammar (cadr e) non-terminal))]
               [else  #f] 
               )
 
         )
-      (cond [(number? e) #t]
-            [(eq? e 'ε)  #f]
+      (cond [(number? e) '(1 (term f))] 
+            [(eq? e 'ε)  (term 0)]
             [(not (eq? grammar '∅)) (if (verifica-list-nonterminal grammar e non-terminal)
                                         (is-WF grammar (lookup-nt grammar e) (append non-terminal e)) 
                                         #f)] 
@@ -45,7 +71,7 @@
   )
 
 (define nt '())
-(define (verf-judg-nt grammar exp) ;; aqui a gente verifica se consome as coisas 
+(define (verf-judg-nt grammar exp) 
 
   (define result (judgment-holds (lookup ,grammar ,exp R) R)) ;; trocar para uma funcao em racket
   
@@ -63,8 +89,8 @@
             [exp-nt (second grammar)]
             )
         (if (eq? nt snt)
-             exp-nt
-             (lookup-nt (third grammar) snt))
+            exp-nt
+            (lookup-nt (third grammar) snt))
         )
       )
   )
@@ -86,7 +112,7 @@
       (let ((id (car e)))
         (cond [(eq? id '/)  (and (is-WF grammar (cadr e) non-terminal) (is-WF grammar (caddr e) non-terminal))] ;; aqui teria que ser um or, nao?
               [(eq? id '•)  (and (is-WF grammar (cadr e) non-terminal)
-                                 (or (zero⇀? grammar (cadr e))
+                                 (or (zero⇀? grammar (cadr e)) ;; precisa desse or?
                                      (is-WF grammar (caddr e) non-terminal)))]
               [(eq? id '!)  (is-WF grammar (cadr e) non-terminal)]
               [(eq? id '*)  (and (is-WF grammar (cadr e) non-terminal)
@@ -109,6 +135,7 @@
 
 (is-WF '(A (/ 0 B) (B 0 ∅)) '(* A) '())
 ;(is-WF '(A (/ 0 B) (B A) ∅) '(* A) '())
+;(is-WF '∅ '(*(/ ε 1)) '())
 
 ; Start function
 (define (test-WF e)
